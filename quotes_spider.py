116b53ef2e33c8919015d797a5f8f6db454b1f0a
Salim Nasir <salim.nasir0057@gmail.com>
2019-10-25 23:28:53 +0500

    added quotes_spider

---
+++ quotes_spider.py
@@ -0,0 +1,23 @@
+import scrapy
+from ..items import QuotetutorialItem
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'http://quotes.toscrape.com/'
+    ]
+
+    def parse(self, response):
+
+        items = QuotetutorialItem()
+        all_div_quotes = response.css('div.quote')
+
+        for quotes in all_div_quotes:
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('small.author::text').extract()
+            tag = quotes.css('a.tag::text').extract()
+
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+            yield items
